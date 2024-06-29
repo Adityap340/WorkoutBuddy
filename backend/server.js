4bfdba813e2f8f48ec1136c1078117ebb7250578
Adityap340 <patiladitya340@gmail.com>
2024-06-29 23:58:52 +0530

    Authentication and Encryption

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 const app = express();
 
 //middleware
@@ -13,7 +14,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/workouts', workoutRoutes)
-
+app.use('/api/users', userRoutes)
 //connect to mongodb
 mongoose.connect(process.env.MONG_URI)
     .then(() => {
