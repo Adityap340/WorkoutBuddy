5be7ef01fc55f04957b5e04b76a1858287d43395
Adityap340 <patiladitya340@gmail.com>
2024-06-30 12:23:33 +0530

    Sigining tokens and login auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -45,4 +45,31 @@ userSchema.statics.signup = async function (email, password) {
     return user
 }
 
+//static login method 
+userSchema.statics.login = async function (email, password) {
+
+    //validation
+    if(!email || !password) {
+        throw Error('All fields are required!')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Invalid Email!')
+    }
+
+    const user = await this.findOne({ email })
+
+    if(!user){
+        throw Error('User does not exist!')  
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error('Invalid credentials!')
+    }
+
+    return user
+
+}
+
 module.exports = mongoose.model('User', userSchema)
